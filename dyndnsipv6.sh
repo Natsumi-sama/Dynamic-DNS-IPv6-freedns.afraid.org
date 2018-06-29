22cca304a0f2d9b060d2d1c9b8cf71bea11a40b6
Chris Cooper <cmcooper123@hotmail.com>
2018-06-30 02:37:11 +1200

    Update dyndnsipv6.sh

--- dyndnsipv6.sh
+++ dyndnsipv6.sh
@@ -3,47 +3,48 @@ IFS=$'\r\n' GLOBIGNORE='*' command eval "IParray=($(ifconfig eth0 | awk '/inet6 
 
 for i in "${IParray[@]}"
 do
-	echo $i
-	if [ "${i:0:4}" = "fe80" ]
-	then
-		echo "link-local address"
-		break
-	fi
-	if ping -c1 -I eth0 $i &> /dev/null
-	then
-		echo "ping successful"
-		IP=$(echo $i | cut -c -14)
-	else
-		echo "deleted $i"
-		sudo ip -6 addr del $i/64 dev eth0
-	fi
+        echo $i
+        if [ "${i:0:4}" = "fe80" ]
+        then
+                echo "link-local address"
+                break
+        fi
+		#find the colon
+		IPTemp=$(echo $i | cut -c -14)
+		if [ "${IPTemp: -1}" = ":" ]
+		then
+				IPTemp="$(echo $IPTemp | cut -c -13)"
+		else
+				if [ "$(echo ${IPTemp: -2} | cut -c 1)" = ":" ]
+				then
+						IPTemp="$(echo $IPTemp | cut -c -12)"
+				fi
+		fi
+        if ping -c1 -I eth0 $IPTemp:1:0000:0000:0000:0000 &> /dev/null
+        then
+                echo "ping successful"
+                IP=$IPTemp
+        else
+                echo "deleted $i"
+                sudo ip -6 addr del $i/64 dev eth0
+        fi
 done
 
 if [ -z "$IP" ]
 then
-	echo "\$IP is empty, there is no IPv6 address"
-	exit
+        echo "\$IP is empty, there is no IPv6 address"
+        exit
 fi
 
-#find the colon
-IP=$(echo $IP | cut -c -14)
-if [ "${IP: -1}" = ":" ]
-then
-	IP="$(echo $IP | cut -c -13)"
-else
-	if [ "$(echo ${IP: -2} | cut -c 1)" = ":" ]
-	then
-		IP="$(echo $IP | cut -c -12)"
-	fi
-fi
 OLDIP="$(cat ipv6.txt)"
-
 if [ "$IP" != "$OLDIP" ]
 then
-	curl "https://freedns.afraid.org/dynamic/update.php?KEY=&address=$IP::1"
-	sudo sed -i 's/'$(echo $OLDIP)'/'$(echo $IP)'/g' /etc/httpd/conf/httpd.conf
+        curl "https://freedns.afraid.org/dynamic/update.php?KEY=&address=$IP:1:0000:0000:0000:0000"
+        sudo sed -i 's/'$(echo $OLDIP)'/'$(echo $IP)'/g' /etc/httpd/conf/httpd.conf
+        sudo sed -i 's/'$(echo $OLDIP)'/'$(echo $IP)'/g' /var/named/domain
+        sudo systemctl restart named
 	sudo systemctl restart httpd
-	echo $IP > ipv6.txt
+        echo $IP > ipv6.txt
 else
-	echo "IPv6 address is the same, no update required"
+        echo "IPv6 address is the same, no update required"
 fi
